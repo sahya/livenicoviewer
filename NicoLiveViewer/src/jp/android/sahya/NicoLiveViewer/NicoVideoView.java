87c39556f2fdaef9022504a443af8d43ff4904e4
tao <tao@taonico>
2012-02-05 17:06:12 +0900

    new NicoVideoView

--- NicoLiveViewer/src/jp/android/sahya/NicoLiveViewer/NicoVideoView.java
+++ NicoLiveViewer/src/jp/android/sahya/NicoLiveViewer/NicoVideoView.java
@@ -9,9 +9,11 @@ import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class NicoVideoView {
-	private final String CONNECT_URL = "http://sp.live.nicovideo.jp/";
+	private final static String CONNECT_URL = "http://sp.live.nicovideo.jp/";
+	private String _loadUrl = "";
 	private WebView webview = null;
 	private PlayerStatusData _playerStatusData = null;
 	private CookieStore cookieStore = null;
@@ -19,8 +21,12 @@ public class NicoVideoView {
 	private CookieManager cookieManager = null;
 
 	public NicoVideoView(WebView webview, CookieStore cookieStore) {
+		this(webview, cookieStore, CONNECT_URL);
+	}
+	public NicoVideoView(WebView webview, CookieStore cookieStore, String loadUrl) {
 		this.webview = webview;
 		this.cookieStore = cookieStore;
+		this._loadUrl = loadUrl;
 		
 		WebSettings settings=webview.getSettings();
 		settings.setPluginsEnabled(true);
@@ -29,28 +35,56 @@ public class NicoVideoView {
 		
 		cookieSyncManager = CookieSyncManager.createInstance(this.webview.getContext());
 		cookieSyncManager.startSync();
-		cookieManager = CookieManager.getInstance();
-		cookieManager.setAcceptCookie(true);
-		cookieManager.removeExpiredCookie();
-		
-		setCookie();
-		this.webview.loadUrl(CONNECT_URL);
+
+		this.webview.setWebViewClient(new NicoWebViewClient());
+	}
+	public void loadUrl(){
+		loadUrl(this._loadUrl);
+	}
+	public void loadUrl(String loadUrl){
+		this.webview.loadUrl(loadUrl);
 	}
 	
-	private void setCookie(){
-		Cookie cookie = null;
-		if ( this.cookieStore != null ) {
-			List<Cookie> cookies = this.cookieStore.getCookies();
-			if (!cookies.isEmpty()) {
-				for (int i = 0; i < cookies.size(); i++) {
-					cookie = cookies.get(i);
-				}
-			}
-			if (cookie != null) {
-				String cookieString = cookie.getName() + "=" + cookie.getValue() + "; domain=" + cookie.getDomain();
-				cookieManager.setCookie( "nicovideo.jp", cookieString);
-				cookieSyncManager.getInstance().sync();
-			}
+	class NicoWebViewClient extends WebViewClient{
+		String loginCookie = null;
+		
+		public NicoWebViewClient (){
+			setCookieManeger();
+    		setCookie();
 		}
-	}
+	
+    	@Override
+    	public void onLoadResource(WebView wv, String url){
+    		setCookieManeger();
+    		setCookie();
+    	}
+    	@Override
+    	public void onPageFinished(WebView wv, String url){
+    		setCookieManeger();
+    		cookieManager.setCookie("nicovideo.jp", loginCookie);
+    	}
+    	
+    	private void setCookie(){
+    		Cookie cookie = null;
+    		if ( cookieStore != null ) {
+    			List<Cookie> cookies = cookieStore.getCookies();
+    			if (!cookies.isEmpty()) {
+    				for (int i = 0; i < cookies.size(); i++) {
+    					cookie = cookies.get(i);
+    				}
+    			}
+    			if (cookie != null) {
+    				loginCookie = cookie.getName() + "=" + cookie.getValue() + "; domain=" + cookie.getDomain();
+    				cookieManager.setCookie( "nicovideo.jp", loginCookie);
+    				cookieSyncManager.sync();
+    			}
+    		}
+    	}
+    	
+    	private void setCookieManeger(){
+    		cookieManager = CookieManager.getInstance();
+    		cookieManager.setAcceptCookie(true);
+    		cookieManager.removeExpiredCookie();
+    	}
+    }
 }
